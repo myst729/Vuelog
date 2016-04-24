5d63c1f34939cb92d1a073c679914767621b3b57
Leo Deng <myst.dg@gmail.com>
2016-04-24 19:35:19 +0800

    fix scrollingElement compatibility

--- src/Vuelog.vue
+++ src/Vuelog.vue
@@ -20,11 +20,27 @@
       }
     },
 
+    methods: {
+      scrollTop () {
+        if (document.scrollingElement && document.scrollingElement.scrollTop) {
+          document.scrollingElement.scrollTop = 0
+          return
+        }
+
+        if (document.documentElement && document.documentElement.scrollTop) {
+          document.documentElement.scrollTop = 0
+          return
+        }
+
+        document.body.scrollTop = 0
+      }
+    },
+
     events: {
       'update-document-title': function (label) {
         var title = this.database.deployment.title
         document.title = label ? `${title} | ${label}` : title
-        document.scrollingElement.scrollTop = 0
+        this.scrollTop()
       }
     }
   }
