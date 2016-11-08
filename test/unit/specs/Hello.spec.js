4702745f0baea6332e558998daeb25b3e390c377
Leo Deng <myst.dg@gmail.com>
2016-11-08 15:06:02 +0800

    update dev env

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -7,6 +7,7 @@ describe('Hello.vue', () => {
       el: document.createElement('div'),
       render: (h) => h(Hello)
     })
-    expect(vm.$el.querySelector('.hello h1').textContent).to.equal('Hello Vue!')
+    expect(vm.$el.querySelector('.hello h1').textContent)
+      .to.equal('Welcome to Your Vue.js App')
   })
 })
