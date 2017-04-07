30403b9f8aa82432440322200d0c954c345e57a7
Leo <Leo@Leo.local>
2017-04-07 21:07:35 +0800

    update deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 > All notable changes to the Vuelog project after v0.4.0 are documented.
 
+### 2.10.1 - 2017-04-07
+
+#### Added
+- Support end to end tests for Microsoft Edge on Windows 10
+
 ### 2.10.0 - 2017-02-27
 
 #### Changed
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -37,7 +37,7 @@ module.exports = {
       {
         test: /\.(js|vue)$/,
         loader: 'eslint-loader',
-        enforce: "pre",
+        enforce: 'pre',
         include: [resolve('src'), resolve('test')],
         options: {
           formatter: require('eslint-friendly-formatter')
@@ -56,7 +56,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
@@ -64,7 +64,7 @@ module.exports = {
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
