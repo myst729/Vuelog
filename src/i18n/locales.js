557a8d669819311fcf9d68e322cbffa8bb0f13ad
danmcatee <danielmcatee@me.com>
2017-11-09 03:28:59 +0100

    Extend locales.js

--- src/i18n/locales.js
+++ src/i18n/locales.js
@@ -1,12 +1,15 @@
-import enUS from './locale_en-us'
-import zhCN from './locale_zh-cn'
+import enUS from './locale_en-us';
+import zhCN from './locale_zh-cn';
+import deDE from './locale_de-de';
 
 export const languages = {
   'en-US': 'English',
-  'zh-CN': '简体中文'
-}
+  'zh-CN': '简体中文',
+  'de-DE': 'Deutsch'
+};
 
 export const locales = {
   'en-US': enUS,
-  'zh-CN': zhCN
-}
+  'zh-CN': zhCN,
+  'de-DE': deDE
+};
