67fee32348b06339cbc444a549faa94196dc2ee9
danmcatee <danielmcatee@me.com>
2017-11-09 03:26:40 +0100

    Add german locale file

---
+++ src/i18n/locale_de-de.js
@@ -0,0 +1,48 @@
+const deDE = {
+  home: {
+    description: 'Ein backend-loses blog system auf Basis von Vue.js',
+    download: 'Download v{0}',
+    github: 'GitHub'
+  },
+
+  oops: {
+    title: 'Oops!',
+    description: 'Die Seite die du suchst existiert nicht.',
+    tip: 'Bring mich nach Hause'
+  },
+
+  archive: {
+    title: 'Archiv',
+    empty: 'Keine Beiträge gefunden.',
+    inCategory: 'Beiträge in Kategorie {0}:',
+    inYear: 'Beiträge im Jahr {0}:',
+    byCategory: 'Beiträge nach Kategorie:',
+    byYear: 'Beiträge nach Jahr:',
+    pages: 'Seiten:'
+  },
+
+  reading: {
+    blog: 'Blog',
+    continued: 'Weiterlesen ...',
+    page: 'Seite {0}'
+  },
+
+  time: {
+    future: 'Irgendwo in der Zeit ( {then} )',
+    yearsAgo: 'vor {diff} Jahren ( {then} )',
+    lastYear: 'Letztes Jahr ( {then} )',
+    monthsAgo: 'vor {diff} Monaten ( {then} )',
+    lastMonth: 'Letzten Monat ( {then} )',
+    daysAgo: 'vor {diff} Tagen ( {then} )',
+    yesterday: 'Gestern ( {then} )',
+    today: 'Heute ( {then} )'
+  },
+
+  lang: {
+    switch: 'Sprache:'
+  },
+
+  credit: 'Gebaut mit {1} und {0}'
+};
+
+export default deDE;
